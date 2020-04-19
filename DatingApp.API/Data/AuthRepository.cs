4f11760569fbd8df18973c4799edb7d6ebc933a5
Veso <Ambroza8888@gmail.com>
2020-04-18 21:38:05 -0700

    First Commit

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+            
+            if(user == null)
+                return null;
+                
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            throw new NotImplementedException();
+        }
+
+    public async Task<User> Register(User user, string password)
+        {
+            // Create 2 bytes[] with passwordHASH and passwordSALT
+            // then we send those bytes[] in the method CREATEPASSWORDHASH
+            // We use "out" keyword so we can send them as empty and they have been refrenced
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        //Created CreatePasswordHash method from the register(keeping the rull of tumb to seperate functions);
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+    public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
