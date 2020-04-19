395aa976bba2b039945e59d326ff2818ac2f3354
Veso <Ambroza8888@gmail.com>
2020-04-18 22:04:30 -0700

    Login done

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -21,11 +21,21 @@ namespace DatingApp.API.Data
                 
             if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
                 return null;
+            
+            return user;
         }
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            throw new NotImplementedException();
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var ComputeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i =0 ; i < ComputeHash.Length; i++)
+                {
+                    if(ComputeHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
         }
 
     public async Task<User> Register(User user, string password)
@@ -33,7 +43,8 @@ namespace DatingApp.API.Data
             // Create 2 bytes[] with passwordHASH and passwordSALT
             // then we send those bytes[] in the method CREATEPASSWORDHASH
             // We use "out" keyword so we can send them as empty and they have been refrenced
-            byte[] passwordHash, passwordSalt;
+            byte[] passwordHash;
+            byte[] passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
