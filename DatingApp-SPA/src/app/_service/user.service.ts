5b8456c0c57532d9d45b618d6f15c74c5700c9b1
Veso <Ambroza8888@gmail.com>
2020-04-25 23:49:05 -0700

    fixed issue with the user token in logIN

--- DatingApp-SPA/src/app/_service/user.service.ts
+++ DatingApp-SPA/src/app/_service/user.service.ts
@@ -4,12 +4,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    // don't forget to add space between Bearer and the tokaen : ('Bearer ')
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -20,9 +14,9 @@ export class UserService {
 constructor(private http: HttpClient) { }
 
   GetUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl + 'users');
   }
   GetUser(id: number): Observable<User> {
-    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 }
