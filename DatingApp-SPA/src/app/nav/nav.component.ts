fa3e0c47c1948a7f5bfbcaeff752f9b7c9981127
Veso <Ambroza8888@gmail.com>
2020-05-30 15:43:05 -0700

    spacing

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -12,7 +12,10 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
+  constructor(
+      public authService: AuthService,
+      private alertify: AlertifyService,
+      private router: Router) { }
 
   ngOnInit() {
   }
