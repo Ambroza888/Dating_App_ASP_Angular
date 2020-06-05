2a6e3a44b4ffed974b59114db2710bd0032a6353
Veso <Ambroza8888@gmail.com>
2020-06-04 23:51:28 -0700

    added cloudinary to the PhotoController

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using CloudinaryDotNet;
 using DatingApp.API.Data;
 using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
@@ -7,20 +8,28 @@ using Microsoft.Extensions.Options;
 
 namespace DatingApp.API.Controllers
 {
-[Authorize]
-[Route("api/users/{userId}/photos")]
-[ApiController]
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
 
-public class PhotosController : ControllerBase
-{
-    private readonly IDatingRepository _repo;
-    private readonly IMapper _mapper;
-    private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
-    public PhotosController(IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+    public class PhotosController : ControllerBase
     {
-        _cloudinaryConfig = cloudinaryConfig;
-        _mapper = mapper;
-        _repo = repo;
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        public readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        public PhotosController(IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            _cloudinaryConfig = cloudinaryConfig;
+            _mapper = mapper;
+            _repo = repo;
+
+            Account acc = new Account
+            (
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+            _cloudinary = new Cloudinary(acc); 
+        } 
     }
-}
 }
