b61bbce519c8c6d2065c2d14b0dcbb67070acc86
Veso <Ambroza8888@gmail.com>
2020-06-05 18:06:33 -0700

    before reinstalling cloudinary

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -55,7 +55,7 @@ namespace DatingApp.API.Controllers
         // Adding photo to User
         // ---------------------------------------------------------------------
         [HttpPost]
-        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto)
+        public async Task<IActionResult> AddPhotoForUser(int userId, [FromForm]PhotoForCreationDto photoForCreationDto)
         {
             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
