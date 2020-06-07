d4a4204c35c54eb0e79e3e69d48382020407bec5
Veso <Ambroza8888@gmail.com>
2020-06-06 17:52:31 -0700

    added ng2-file-upload

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from 'src/app/_models/photo';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-photo-editor',
@@ -7,10 +8,17 @@ import { Photo } from 'src/app/_models/photo';
   styleUrls: ['./photo-editor.component.css']
 })
 export class PhotoEditorComponent implements OnInit {
-@Input() photos: Photo[];
+  @Input() photos: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
   }
 
 }
