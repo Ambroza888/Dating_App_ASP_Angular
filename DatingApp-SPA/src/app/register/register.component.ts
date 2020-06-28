b9c0ebe91450e040c84877aa6a8868cb0954fc91
Veso <Ambroza8888@gmail.com>
2020-06-27 18:42:46 -0700

    Added reacted form

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_service/Auth.service';
 import { AlertifyService } from '../_service/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +11,28 @@ import { AlertifyService } from '../_service/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelFromRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
+
+
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+
+  this.registerForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    confirmPassword: new FormControl()
+  });
+
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('reg successfull');
-    }, error => {
-      this.alertify.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('reg successfull');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value);
   }
   cancel() {
     this.cancelFromRegister.emit(false);
