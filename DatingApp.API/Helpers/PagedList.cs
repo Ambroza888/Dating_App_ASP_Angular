de3b615fd9ee981484c54a9dc62d9041e36d18cb
Veso <Ambroza8888@gmail.com>
2020-10-21 17:23:55 -0700

    Finished class for pagedList

--- DatingApp.API/Helpers/PagedList.cs
+++ DatingApp.API/Helpers/PagedList.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Helpers
 {
@@ -16,6 +19,13 @@ namespace DatingApp.API.Helpers
             CurrentPage = pageNumber;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             this.AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber-1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
 }
