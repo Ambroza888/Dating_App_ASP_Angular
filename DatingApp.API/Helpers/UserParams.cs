ea64dd79311bd21a8ada5895fc58c3771bd2fed0
Veso <Ambroza8888@gmail.com>
2020-10-24 03:03:14 -0700

    Little readable trick

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -4,12 +4,12 @@ namespace DatingApp.API.Helpers
     {
         private const int MaxPageSize = 50;
         private int pageSize = 10;
-        public int PageNumber { get; set; } = 1;
         public int PageSize
         {
             get { return pageSize; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
+        public int PageNumber { get; set; } = 1;
         public int UserId { get; set; }
         public string Gender { get; set;}
         public int MinAge { get; set; } = 18;
